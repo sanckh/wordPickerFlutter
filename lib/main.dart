43c31d9d1e38fdd6c4a9e8a8f74a21d7af005cf7
sanckh <csutton@student.fullsail.edu>
2023-09-09 16:16:09 -0500

    broke out the home widget into two so i could add a drawer.

--- lib/main.dart
+++ lib/main.dart
@@ -13,16 +13,14 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return ChangeNotifierProvider(
-      create: (context) => MyAppState(),
-      child: MaterialApp(
-        title: 'Namer Application',
-        theme: ThemeData(
-          useMaterial3: true,
-          colorScheme: ColorScheme.fromSeed(seedColor: Colors.cyan)
-        ),
-        home: MyHomePage(),
-      )
-    );
+        create: (context) => MyAppState(),
+        child: MaterialApp(
+          title: 'Namer Application',
+          theme: ThemeData(
+              useMaterial3: true,
+              colorScheme: ColorScheme.fromSeed(seedColor: Colors.cyan)),
+          home: MyHomePage(),
+        ));
   }
 }
 
@@ -33,26 +31,119 @@ class MyAppState extends ChangeNotifier {
     current = WordPair.random();
     notifyListeners();
   }
+
+  var favorites = <WordPair>[];
+
+  void toggleFavorite() {
+    if (favorites.contains(current)) {
+      favorites.remove(current);
+    } else {
+      favorites.add(current);
+    }
+    notifyListeners();
+  }
 }
 
-
 class MyHomePage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Row(
+        children: [
+          SafeArea(
+            child: NavigationRail(
+              extended: false,
+              destinations: [
+                NavigationRailDestination(
+                  icon: Icon(Icons.home),
+                  label: Text('Home'),
+                ),
+                NavigationRailDestination(
+                  icon: Icon(Icons.favorite),
+                  label: Text('Favorite'),
+                ),
+              ],
+              selectedIndex: 0,
+              onDestinationSelected: (value) {
+                print(' selected: $value');
+              }
+            ),
+          ),
+          Expanded(
+            child: Container(
+              color: Theme.of(context).colorScheme.primaryContainer,
+              child: GeneratorPage(),
+            )
+          )
+        ],
+      )
+    );
+  }
+}
+
+class GeneratorPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     var appState = context.watch<MyAppState>();
     var pair = appState.current;
     final theme = Theme.of(context);
-    return Scaffold(
-      backgroundColor: theme.colorScheme.background,
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            Text('A random idea:'),
+
+    IconData icon;
+    if(appState.favorites.contains(pair)){
+      icon = Icons.favorite;
+    } else{
+      icon = Icons.home;
+    }
+    return Center(
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
             BigCard(pair: pair),
-            NextButtonWidget(appState: appState)
-          ],
-        ),
+            SizedBox(height: 10),
+            Row(
+              mainAxisSize: MainAxisSize.min,
+              children: [
+                NextButtonWidget(appState: appState),
+                FavoriteButtonWidget(appState: appState, pair: pair),
+              ],
+            )
+        ]
+      )
+    );
+  }
+}
+
+class FavoriteButtonWidget extends StatelessWidget {
+  const FavoriteButtonWidget({
+    super.key,
+    required this.appState,
+    required this.pair,
+  });
+
+  final MyAppState appState;
+  final WordPair pair;
+
+  @override
+  Widget build(BuildContext context) {
+    IconData icon;
+    if (appState.favorites.contains(pair)) {
+      icon = Icons.favorite;
+    } else {
+      icon = Icons.favorite_border;
+    }
+    final theme = Theme.of(context);
+    return ElevatedButton.icon(
+      onPressed: () {
+        appState.toggleFavorite();
+      },
+      icon: Icon(icon),
+      label: Text("Like",
+          style: theme.textTheme.displaySmall!.copyWith(
+            color: theme.colorScheme.onPrimary,
+          )),
+      style: ButtonStyle(
+        backgroundColor: MaterialStateProperty.all(theme.colorScheme.tertiary),
+        foregroundColor: MaterialStateProperty.all(theme.colorScheme.onPrimary),
       ),
     );
   }
@@ -68,11 +159,20 @@ class NextButtonWidget extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    final theme = Theme.of(context);
+    final style = theme.textTheme.displaySmall!.copyWith(
+      color: theme.colorScheme.onPrimary,
+    );
     return ElevatedButton(
       onPressed: () {
         appState.getNext();
       },
-      child: Text("Next"),
+      style: ElevatedButton.styleFrom(
+          backgroundColor: theme.colorScheme.secondary),
+      child: Text(
+        "Next",
+        style: style,
+      ),
     );
   }
 }
@@ -100,10 +200,8 @@ class BigCard extends StatelessWidget {
           pair.asLowerCase,
           style: style,
           semanticsLabel: "${pair.first} ${pair.second}",
-          ),
+        ),
       ),
     );
   }
 }
-
- 
